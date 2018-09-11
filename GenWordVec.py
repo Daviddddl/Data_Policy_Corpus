77e54728f7c0feccad95aa01641216c5452d7cfe
daviddddl <daviddl997004866@gmail.com>
2018-09-11 13:51:20 +0800

    新增百度爬虫

--- GenWordVec.py
+++ GenWordVec.py
@@ -5,18 +5,18 @@ vec_model_path = './data/corpusWord2Vec.bin'
 tmp_file_path = './data/tmp.txt'
 
 
-def genWord4Vec(content):
+def genVecFromStr(content):
     with open(tmp_file_path, 'w+') as f:
         f.write(content)
     word2vec.word2vec(tmp_file_path, vec_model_path, size=300, verbose=True)
 
 
+def genVecFromFile(file):
+    word2vec.word2vec(file, vec_model_path, size=300, verbose=True)
+
+
 if __name__ == '__main__':
 
-    LTPez.LTP_parse_content()
-    genWord4Vec('大家 好！')
+    genVecFromFile('./data/ltpres.txt')
     model = word2vec.load(vec_model_path)
     print(model.vectors)
-    indexes = model.cosine(u'我')
-    for index in indexes[0]:
-        print(model.vocab[index])
